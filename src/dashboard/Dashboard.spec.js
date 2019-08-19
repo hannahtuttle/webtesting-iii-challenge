601a49992b18772fe3f288d8358cbef3a71f8c75
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-08-19 12:59:49 -0700

    adding snapshot testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  // write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
